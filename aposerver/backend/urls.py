1134010dd41064b0be386fb64dd457b0dd6280f2
reritom <reikudjinn@gmail.com>
2019-01-13 18:17:50 +0100

    Django aposerver removed

--- aposerver/backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from backend.views.accounts import login_user, logout_user, signup_user
-
-urlpatterns = [
-    path('login/', login_user),
-    path('logout/', logout_user),
-    path('signup/', signup_user)
-]
